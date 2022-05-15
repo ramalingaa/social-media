e1ef690d0d13682fdcae37dd5301df76f2c032ec
ramalingaa <87629304+ramalingaa@users.noreply.github.com>
2022-05-15 23:28:14 +0530

    Docs

--- src/frontend/components/navbar/UserProfile.jsx
+++ src/frontend/components/navbar/UserProfile.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import { useClickOutside } from '../../../customeHooks/useClickOutside';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { postActions } from './../../../redux store/postSlice';
 
 const UserProfile = ({setProfileDisplay}) => {
-    const { userProfileData, setJwtToken } = useSelector((store) => store.post)
-
+    const { userProfileData } = useSelector((store) => store.post)
+    const dispatch = useDispatch()
     const  navigate  = useNavigate()
     const logoutUser = () => {
         localStorage.removeItem("ONE")
-        setJwtToken(() => "")
+        dispatch(postActions.getJwtToken(""))
         setProfileDisplay((prev) => !prev)
         navigate("/login")
       }
